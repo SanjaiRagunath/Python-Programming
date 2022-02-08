7b0a5cf103332f83e615a9a59065af8c4fa62be0
SanjaiRagunath <99281175+SanjaiRagunath@users.noreply.github.com>
2022-02-08 23:30:24 +0530

    Create Radius of a circle and File extension.py

---
+++ Radius of a circle and File extension.py
@@ -0,0 +1,11 @@
+Radius of a Circle :
+
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+File Extension :
+
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
